ac1dd26a146d350ee3c126340b3beb63541818f9
Aman Kumar Arya <amanarya0742@gmail.com>
2026-05-19 16:16:30 +0530

    Added change password API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -109,10 +109,97 @@ const loginAdmin =
 
     }
 
-  };
+};
+
+const changePassword =
+  async (req, res) => {
+
+    console.log("Change password API hit");
+
+    try {
+
+      const {
+
+        currentPassword,
+
+        newPassword,
+
+      } = req.body;
+
+      const admin =
+        await Admin.findById(
+          req.user.id
+        );
+
+      if (!admin) {
+
+        return res.status(404).json({
+
+          message:
+            "User not found",
+
+        });
+
+      }
+
+      const isMatch =
+        await bcrypt.compare(
+
+          currentPassword,
+
+          admin.password
+
+        );
+
+      if (!isMatch) {
+
+        return res.status(400).json({
+
+          message:
+            "Current password is incorrect",
+
+        });
+
+      }
+
+      const hashedPassword =
+        await bcrypt.hash(
+
+          newPassword,
+
+          10
+
+        );
+
+      admin.password =
+        hashedPassword;
+
+      await admin.save();
+
+      res.json({
+
+        message:
+          "Password updated successfully",
+
+      });
+
+    } catch (error) {
+
+      res.status(500).json({
+
+        message:
+          error.message,
+
+      });
+
+    }
+
+};
 
 module.exports = {
 
   loginAdmin,
+
+  changePassword,
 
 };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,9 +3,29 @@ const express = require("express");
 const router = express.Router();
 
 const {
+
   loginAdmin,
+
+  changePassword,
+
 } = require("../controllers/authController");
 
-router.post("/login", loginAdmin);
+const authMiddleware =
+  require("../middleware/authMiddleware");
+
+router.post(
+  "/login",
+  loginAdmin
+);
+
+router.put(
+
+  "/change-password",
+
+  authMiddleware,
+
+  changePassword
+
+);
 
 module.exports = router;
