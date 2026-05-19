418c79bc0da210bac03befb009d12e31b85972b0
Aman Kumar Arya <amanarya0742@gmail.com>
2026-05-19 12:53:55 +0530

    Connected frontend with Render backend and MongoDB Atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log("MongoDB Connected");
+  try {
+
+    await mongoose.connect(
+      process.env.MONGO_URI
+    );
+
+    console.log("MongoDB Atlas Connected");
+
   } catch (error) {
+
     console.log(error);
+
     process.exit(1);
+
   }
+
 };
 
 module.exports = connectDB;
--- frontend/src/app/lib/axios.ts
+++ frontend/src/app/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
 
   baseURL:
-    "http://localhost:5000",
+    (import.meta as any).env.VITE_API_URL,
 
   headers: {
 
